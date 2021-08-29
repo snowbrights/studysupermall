71df748697c500044c0b685af39c27a99768d55c
snowbrights <snowbrights@163.com>
2021-08-30 02:20:54 +0800

    提交

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <div id="app">
+    <router-view/>
+    <main-tab-bar/>
   </div>
 </template>
 
 <script>
+import MainTabBar from 'components/content/mainTabbar/MainTabBar'
 export default {
   name: 'App',
   components: {
